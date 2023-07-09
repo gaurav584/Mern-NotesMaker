393d91b261a9201af8c5e089f0ede963a96803f8
gauravyadav3345 <gauravyadav3445@gmail.com>
2023-07-09 22:34:48 +0530

    first-commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+
+
+const app = express();
+dotenv.config();
+
+app.get("/",(req,res)=>{
+    console.log("API is working..");
+})
+
+app.get("api/notes",(req,res)=>{
+    res.json(notes);
+})
+
+app.get("/api/notes/:id",(req,res)=>{
+    const notes = notes.find((n)=> n._id === req.params.id);
+    
+    res.send(note);
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,console.log(`server started on port ${PORT}`));
